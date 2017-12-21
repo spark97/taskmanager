14f7f9a14bf81df6cf9ed5a4d6ce8903e1016606
hsallbd@gmail.com <hsallbd@gmail.com>
2017-12-21 13:37:48 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+
+  document.getElementById("myUL").appendChild(li);
+  document.getElementById("myInput").value = "";
+}
